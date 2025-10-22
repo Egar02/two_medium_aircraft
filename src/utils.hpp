875edbab9aa0c3949b3b4da979e85ada2ef18f54
egor.tomskikh <etomskikh02@gmail.com>
2025-10-22 15:58:08 +0300

    Добавлено автоформатирование кода

--- src/utils.hpp
+++ src/utils.hpp
@@ -6,8 +6,8 @@
 #include <nlohmann/json.hpp>
 #include <filesystem>
 
-void read_array_from_file(std::string&& file_path, std::vector<double>& array);
+void read_array_from_file(std::string &&file_path, std::vector<double> &array);
 
-void read_json(std::string&& file_path, nlohmann::json& json_data);
+void read_json(std::string &&file_path, nlohmann::json &json_data);
 
-void apply_func(std::function<double(double)> func, std::vector<double>& array, std::vector<double>& mapped_array);
+void apply_func(std::function<double(double)> func, std::vector<double> &array, std::vector<double> &mapped_array);
